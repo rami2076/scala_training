df95a476a035d3d93ef40ca66da5f36063ed14f1
Tadashi Hashimoto <sakusennx@gmail.com>
2019-11-18 14:45:14 +0900

    add trait base.

---
+++ src/tutorial/Trait_Ex1.scala
@@ -0,0 +1,32 @@
+package tutorial
+
+
+/*
+*
+* 訳：(人・ものの)特性、特色、特徴
+* https://ejje.weblio.jp/content/trait
+*
+*
+*
+*
+ */
+
+//トレイとの特徴
+//・複数のトレイトを一つのクラスやトレイトにミックスインできる
+//・直接インスタンス化できない(検証対象：〇)
+//・クラスパラメータ(コンストラクタの引数)を取ることができない
+
+
+//ref:http://dwango.github.io/scala_text_previews/sbt-tut/trait.html
+
+//ブロックで定義できる
+trait Trait_Ex1 {
+
+}
+
+//ブロックなしでも定義できる
+trait Trait_Ex1_1
+
+
+
+
