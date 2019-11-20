7e8da15067c5ea97cd266c26df0638d2a56c10ac
Tadashi Hashimoto <sakusennx@gmail.com>
2019-11-20 18:49:02 +0900

    fixed comment . refactor中に削除されたコメントの復元

--- src/test/roughly_scala/companion/Test_Object_Companion_Ex1_Factory.scala
+++ src/test/roughly_scala/companion/Test_Object_Companion_Ex1_Factory.scala
@@ -2,6 +2,11 @@ package test.roughly_scala.companion
 
 import tutorial.roughly_scala.companion.Object_Companion_Ex1_Factory
 
+/**
+  * コンパニオン機能の確認
+  * applyの確認
+  */
+
 object Test_Object_Companion_Ex1_Factory extends App {
 
   //http://www.ne.jp/asahi/hishidama/home/tech/scala/def.html
--- src/tutorial/roughly_scala/base/Object_Entry.scala
+++ src/tutorial/roughly_scala/base/Object_Entry.scala
@@ -1,6 +1,15 @@
 package tutorial.roughly_scala.base
 
-//エントリーポイントはObject内のmain(args:Array[String]):Unitと同一のシグネチャを持つ
+//
+
+/**
+  * エントリーポイントはObject内のmain(args:Array[String]):Unitと同一のシグネチャを持つ
+  *
+  *
+  * ObjectはJavaでいう静的クラスに当たる。
+  * /シングルトンオブジェクトの生成ができる。
+  */
+
 object Object_Entry {
   //エントリーポイント
   def main(args: Array[String]): Unit = {
