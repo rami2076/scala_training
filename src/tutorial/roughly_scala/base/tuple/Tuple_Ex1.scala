38500f8977a5ca38a3a89960cb28adbf1028e783
Tadashi Hashimoto <sakusennx@gmail.com>
2019-11-21 19:53:30 +0900

    add Tuple training.

---
+++ src/tutorial/roughly_scala/base/tuple/Tuple_Ex1.scala
@@ -0,0 +1,38 @@
+package tutorial.roughly_scala.base.tuple
+
+/**
+  *
+  * tupleについて
+  * tupleとは複数の構成要素からなる一組の情報を表現するための型
+  *
+  * tuple(名詞)：組、順序
+  *
+  * Scalaでは最大22となっている。
+  *
+  *
+  *
+  */
+
+object Tuple_Ex1 extends App {
+
+  //tupleの初期化
+  val tuple = ("tom", 5)
+  //tupleの文字列化
+  println(tuple)
+  println(tuple.getClass)
+
+  println(tuple._1)
+  println(tuple._1.getClass)
+
+  println(tuple._2)
+  println(tuple._2.getClass)
+
+  val tuple_max = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22")
+  println(tuple_max)
+  println(tuple_max.getClass)
+
+  //Error:(27, 19) too many elements for tuple: 23, allowed: 22
+  //  val tuple_max = ("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23")
+  //val tuple_over = ("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23")
+  //println(tuple_max)
+}
